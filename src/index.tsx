07bbe00202d1720fffbbd3bd13a4dfec64516af3
nlom0218 <nlom0218@naver.com>
2022-03-15 20:37:53 +0900

    Definitely Typed

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
-import App from './App';
+import App from './App'
 
 const darkTheme = {
   textColor: "whitesmoke",
