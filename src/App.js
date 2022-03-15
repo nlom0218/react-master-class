d632b866c808f18988db11c713baf274f9b0344f
nlom0218 <nlom0218@naver.com>
2022-03-15 18:28:31 +0900

    As and Attrs

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import styled from "styled-components";
 
 const Father = styled.div`
@@ -14,13 +15,32 @@ const Circle = styled(Box)`
   border-radius: 50%;
 `
 
+const Btn = styled.div`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`
+
+const Input = styled.input.attrs({ required: true, maxLength: "10" })`
+  background-color: tomato;
+`
 
 function App() {
-  return (
-    <Father>
+  return (<React.Fragment>
+    <Father as="header">
       <Box bgColor="teal" />
       <Circle bgColor="tomato" />
     </Father>
+    <Btn>Log in</Btn>
+    <Btn as="a" href="/">Log in</Btn>
+    <Input />
+    <Input />
+    <Input />
+    <Input />
+    <Input />
+    <Input />
+  </React.Fragment>
   );
 }
 
