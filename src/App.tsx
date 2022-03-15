c09f99bcd156f202dba9b64b2211d6c4558322fa
nlom0218 <nlom0218@naver.com>
2022-03-15 21:13:12 +0900

    Typing the Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
+import Circle from "./Circle";
 
-const Wapper = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: ${props => props.theme.backgroundColor};
-`
-
-const Text = styled.div`
-  color: ${props => props.theme.textColor};
-`
 
 function App() {
-  return (<Wapper>
-    <Text>Hello</Text>
-  </Wapper>
+  return (<div>
+    <Circle bgColor="teal" />
+    <Circle bgColor="tomato" />
+  </div>
   );
 }
 
