91319b55769ea13a83f7f18ffd43063c3b8ab987
nlom0218 <nlom0218@naver.com>
2022-03-19 19:28:12 +0900

    Nested Routes part One

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,11 +3,13 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import Chart from "./Page/Chart";
 import Circle from "./Page/Circle";
 import Coin from "./Page/Coin";
 import Coins from "./Page/Coins";
 import Form from "./Page/Form";
 import Home from "./Page/Home";
+import Price from "./Page/Price";
 
 const Router = () => {
   return <BrowserRouter>
@@ -16,7 +18,10 @@ const Router = () => {
       <Route path="/circle" element={<Circle />} />
       <Route path="/form" element={<Form />} />
       <Route path="/coins" element={<Coins />} />
-      <Route path="/:coinId" element={<Coin />} />
+      <Route path="/:coinId" element={<Coin />} >
+        <Route path="price" element={<Price />} />
+        <Route path="chart" element={<Chart />} />
+      </Route>
     </Routes>
   </BrowserRouter>
 }
