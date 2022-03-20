e01eb240d524e5d6ad304c49d1ba7531f1e0b3db
nlom0218 <nlom0218@naver.com>
2022-03-20 18:08:02 +0900

    Price Chart part Two

--- src/Page/Chart.tsx
+++ src/Page/Chart.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "react-query";
 import { useOutletContext } from "react-router"
 import { fetchCoinHistory } from "../api"
+import ApexChart from "react-apexcharts"
 
 interface ICoinId {
   coinId: string
@@ -24,7 +25,45 @@ const Chart = () => {
   const { isLoading, data } = useQuery<ICoinHistorical[]>(["history", coinId], () => fetchCoinHistory(coinId))
   console.log(data);
 
-  return <h1>Chart</h1>
+  return <div>{isLoading ? "Loading chart..." : <ApexChart
+    type="line"
+    series={[
+      {
+        name: "Pirce",
+        data: data?.map(price => price.close) ?? []
+      }
+    ]}
+    options={{
+      theme: {
+        mode: "dark"
+      },
+      chart: {
+        height: 300,
+        width: 500,
+        background: "transparent"
+      },
+      stroke: {
+        curve: "smooth",
+        width: 4
+      },
+      grid: {
+        show: false
+      },
+      xaxis: {
+        labels: {
+          show: false
+        },
+        categories: data?.map(price => price.time_close) ?? [],
+        axisBorder: {
+          show: false
+        },
+      },
+      yaxis: {
+        labels: {
+          show: false
+        }
+      }
+    }} />}</div>
 }
 
 export default Chart
