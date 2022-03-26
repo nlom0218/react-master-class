2bd32ffed8448ffec960bfe1b1391d374a76fd0f
nlom0218 <nlom0218@naver.com>
2022-03-26 13:21:47 +0900

    Form Errors

--- src/Page/ToDoList.tsx
+++ src/Page/ToDoList.tsx
@@ -1,14 +1,20 @@
 import { useState } from "react"
 import { useForm } from "react-hook-form"
 
-type FormInput = {
+interface FormInput {
   todo: string
   day: string
+  password: string
+  password1: string
 }
 
 const ToDoList = () => {
   const { register, handleSubmit, formState: { errors } } = useForm<FormInput>({
-    mode: "onChange"
+    mode: "onChange",
+    defaultValues: {
+      todo: "오늘 할 일을 적어보세요!",
+      day: new Date() + ""
+    }
   })
 
   const onSubmit = (data: FormInput) => {
@@ -21,21 +27,52 @@ const ToDoList = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <input
         {...register("todo", {
-          required: "Password is required",
+          required: "Todo is required",
           minLength: {
             value: 5,
-            message: "Your password is too short"
+            message: "Your todo is too short"
+          },
+          pattern: {
+            value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+            message: "Only naver.com emails allowed"
           }
         })}
         placeholder="Write a to do"
       />
+      <span>{errors?.todo?.message}</span>
       <input
         {...register("day", {
-          required: true,
-          minLength: 5
+          required: "Day is required",
+          minLength: {
+            value: 5,
+            message: "Your day is too short"
+          }
         })}
         placeholder="Write a day"
       />
+      <span>{errors?.day?.message}</span>
+      <input
+        {...register("password", {
+          required: "Password is required",
+          minLength: {
+            value: 5,
+            message: "Your Password is too short"
+          }
+        })}
+        placeholder="Write a Password"
+      />
+      <span>{errors?.password?.message}</span>
+      <input
+        {...register("password1", {
+          required: "Password1 is required",
+          minLength: {
+            value: 5,
+            message: "Your Password1 is too short"
+          }
+        })}
+        placeholder="Write a Password1"
+      />
+      <span>{errors?.password1?.message}</span>
       <button>Add</button>
     </form>
   </div>
