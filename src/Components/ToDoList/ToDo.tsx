a504ce48f1248a9341ac15b0b1a511be0d993374
nlom0218 <nlom0218@naver.com>
2022-03-27 14:09:04 +0900

    Categories

--- src/Components/ToDoList/ToDo.tsx
+++ src/Components/ToDoList/ToDo.tsx
@@ -1,12 +1,24 @@
-import { useRecoilValue } from "recoil"
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil"
 import { IToDo, toDoState } from "../../atoms"
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const [toDos, setToDos] = useRecoilState(toDoState)
+  const onClickCategory = (category: IToDo["category"]) => {
+    // const newToDos = toDos.map(item => {
+    //   if (item.id === id) {
+    //     return { text: item.text, id: item.id, category }
+    //   } else {
+    //     return item
+    //   }
+    // })
+    // setToDos(newToDos)
+
+  }
   return <li>
     <span>{text}</span>
-    <button>To Do</button>
-    <button>Doing</button>
-    <button>Done</button>
+    {category !== "DOING" && <button onClick={() => onClickCategory("DOING")}>DOING</button>}
+    {category !== "TO_DO" && <button onClick={() => onClickCategory("TO_DO")}>To Do</button>}
+    {category !== "DONE" && <button onClick={() => onClickCategory("DONE")}>DONE</button>}
   </li>
 }
 
