55e0995b61eaac723ffbaa5d086508e69fc1fe09
nlom0218 <nlom0218@naver.com>
2022-03-27 16:49:59 +0900

    Selectors part One

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,4 +1,4 @@
-import { atom, useSetRecoilState } from "recoil";
+import { atom, selector } from "recoil";
 
 export const isDarkAtom = atom({
   key: "isDark",
@@ -14,4 +14,16 @@ export interface IToDo {
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: []
+})
+
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState)
+    return [
+      toDos.filter(toDo => toDo.category === "TO_DO"),
+      toDos.filter(toDo => toDo.category === "DOING"),
+      toDos.filter(toDo => toDo.category === "DONE"),
+    ]
+  }
 })
