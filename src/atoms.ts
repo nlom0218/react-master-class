dc210a8b66cf96865cfec5b0d658b5e18f34d7f9
nlom0218 <nlom0218@naver.com>
2022-03-27 17:12:22 +0900

    Selectors part Two

--- src/atoms.ts
+++ src/atoms.ts
@@ -11,6 +11,11 @@ export interface IToDo {
   id: number
 }
 
+export const categoryState = atom({
+  key: "category",
+  default: "TO_DO"
+})
+
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: []
@@ -20,10 +25,7 @@ export const toDoSelector = selector({
   key: "toDoSelector",
   get: ({ get }) => {
     const toDos = get(toDoState)
-    return [
-      toDos.filter(toDo => toDo.category === "TO_DO"),
-      toDos.filter(toDo => toDo.category === "DOING"),
-      toDos.filter(toDo => toDo.category === "DONE"),
-    ]
+    const category = get(categoryState)
+    return toDos.filter(toDo => toDo.category === category)
   }
 })
