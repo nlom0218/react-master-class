c1692bac3c6effd06d12fcb05d6bd20afa707e0e
nlom0218 <nlom0218@naver.com>
2022-04-04 20:34:39 +0900

    Drag and Drop part One

--- src/Page/Trello.tsx
+++ src/Page/Trello.tsx
@@ -1,42 +1,25 @@
-import { useEffect } from "react"
-import { useForm } from "react-hook-form"
-import { useRecoilState, useRecoilValue } from "recoil"
-import { hourSelector, minuteState } from "../atoms"
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd"
 
-interface IFormInput {
-  minutes: number
-}
+
 
 const Trello = () => {
-  const [minutes, setMinutes] = useRecoilState(minuteState)
-  const [hours, setHours] = useRecoilState(hourSelector)
 
-  const { register, handleSubmit, getValues, setValue } = useForm({
-    mode: "onChange",
-  })
+  const onDragEnd = () => { }
 
-  const onChange = () => {
-    setMinutes(+getValues("minutes"))
-    setHours(+getValues("hours"))
-  }
-
-  useEffect(() => {
-    setValue("hours", hours)
-  }, [minutes])
-
-  return <form>
-    <input
-      {...register("minutes", { onChange })
-      }
-      type="number"
-      placeholder="Minutes"
-    />
-    <input
-      {...register("hours", { onChange })}
-      type="number"
-      placeholder="Hours"
-    />
-  </form>
+  return <DragDropContext onDragEnd={onDragEnd}>
+    <div>
+      <Droppable droppableId="one">
+        {() => <ul>
+          <Draggable draggableId="first" index={0}>
+            {() => <li>ONE</li>}
+          </Draggable>
+          <Draggable draggableId="second" index={1}>
+            {() => <li>TWO</li>}
+          </Draggable>
+        </ul>}
+      </Droppable>
+    </div>
+  </DragDropContext>
 }
 
 export default Trello
