b9cab9c6105b985b21f59b5293b36d23beb9b577
nlom0218 <nlom0218@naver.com>
2022-04-10 13:18:20 +0900

    Basic Animations

--- src/Page/Animations.tsx
+++ src/Page/Animations.tsx
@@ -10,19 +10,22 @@ const Wrapper = styled.div`
   background: linear-gradient(135deg, #e09, #d0e);
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 20px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const Animations = () => {
   return (
     <Wrapper>
-      <Box />
-      <motion.div></motion.div>
+      <Box
+        transition={{ type: "spring", bounce: 0.5, delay: 0.5 }}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotateZ: 360 }}
+      />
     </Wrapper>
   );
 };
