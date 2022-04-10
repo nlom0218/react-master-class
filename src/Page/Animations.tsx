6ee8f42c576e98eb14485b0fa08f37944fc20bb9
nlom0218 <nlom0218@naver.com>
2022-04-10 15:56:38 +0900

    MotionValues part One

--- src/Page/Animations.tsx
+++ src/Page/Animations.tsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
-import { useRef } from "react";
+import { motion, useMotionValue } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 const Wrapper = styled.div`
-  padding: 100px;
+  padding: 50px;
   height: 100vh;
   width: 100vw;
   display: grid;
@@ -15,7 +15,7 @@ const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 20px;
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
@@ -116,6 +116,11 @@ const box3Var = {
 
 const Animations = () => {
   const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  useEffect(() => {
+    x.onChange(() => console.log(x.get()));
+  }, [x]);
+
   return (
     <Wrapper>
       <Box variants={myVars} initial="start" animate="end" />
@@ -136,6 +141,7 @@ const Animations = () => {
           whileDrag="drag"
         />
       </BiggerBox>
+      <Box3 drag="x" dragSnapToOrigin style={{ x }}></Box3>
     </Wrapper>
   );
 };
