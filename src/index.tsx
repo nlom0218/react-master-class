f35a788cf35dba7717df9bda297b786923d1d37e
nlom0218 <nlom0218@naver.com>
2022-05-10 23:55:27 +0900

    Header part One

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,33 @@ import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { createGlobalStyle } from "styled-components";
 import { theme } from "./theme";
+import { Reset } from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
-`;
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+* {
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Source Sans Pro', sans-serif;
+  color:black;
+  line-height: 1.2;
+  
+}
+a {
+  text-decoration:none;
+  color:inherit;
+}`;
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
       <ThemeProvider theme={theme}>
+        <Reset />
         <GlobalStyle />
         <App />
       </ThemeProvider>
