23bb6dcdb6ea32d39ba182c844ccaa1c9cfc553b
nlom0218 <nlom0218@naver.com>
2022-05-14 10:17:01 +0900

    Search Redirect

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import { motion, useAnimation, useViewportScroll } from "framer-motion";
-import { Link, useMatch } from "react-router-dom";
+import { Link, useMatch, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Nav = styled(motion.nav)`
   display: flex;
@@ -49,7 +50,7 @@ const Item = styled.li`
   }
 `;
 
-const Search = styled.span`
+const Search = styled.form`
   color: white;
   display: flex;
   align-items: center;
@@ -105,6 +106,10 @@ const navVariants = {
   },
 };
 
+interface IForm {
+  keyword: string;
+}
+
 function Header() {
   const [searchOpen, setSearchOpen] = useState(false);
   const homeMatch = useMatch("/");
@@ -112,6 +117,7 @@ function Header() {
   const inputAnimtation = useAnimation();
   const navAnimation = useAnimation();
   const { scrollY } = useViewportScroll();
+  const navigate = useNavigate();
 
   const openSearch = () => {
     if (searchOpen) {
@@ -124,6 +130,12 @@ function Header() {
       });
     }
     setSearchOpen((prev) => !prev);
+  };
+
+  const { register, handleSubmit } = useForm<IForm>();
+
+  const onValid = (data: IForm) => {
+    navigate(`/search?keyword=${data.keyword}`);
   };
 
   useEffect(() => {
@@ -162,7 +174,7 @@ function Header() {
         </Items>
       </Col>
       <Col>
-        <Search>
+        <Search onSubmit={handleSubmit(onValid)}>
           <motion.svg
             onClick={openSearch}
             animate={{ x: searchOpen ? -185 : 0 }}
@@ -178,6 +190,7 @@ function Header() {
             ></path>
           </motion.svg>
           <Input
+            {...register("keyword", { required: true, minLength: 2 })}
             animate={inputAnimtation}
             initial={{ scaleX: 0 }}
             transition={{ type: "linear" }}
